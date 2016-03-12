5c5980c19249beab1825555432bfe03420bebcc9
perraid <davidperrai@gmail.com>
2016-03-12 13:40:25 +0100

    correction de la route du file upload

--- restAPI/fileUploadRoutes.js
+++ restAPI/fileUploadRoutes.js
@@ -10,14 +10,16 @@ router.use(bodyParser.urlencoded({
 }));
 
 router.post('/upload/file',function(res,req,next){
+    
+res.send(" "+process.env.OPENSHIFT_DATA_DIR);
  
- fs.writeFile(process.env.OPENSHIFT_DATA_DIR + 'message.txt', 'Hello Node', function (err) {
-    if(err){
-        res.send(err);
-    } else{ 
-        res.send('uploaded');
-    }
-});
+//  fs.writeFile(process.env.OPENSHIFT_DATA_DIR + 'message.txt', 'Hello Node', function (err) {
+//     if(err){
+//         res.send(err);
+//     } else { 
+//         res.send('uploaded');
+//     }
+// });
    
 });
 

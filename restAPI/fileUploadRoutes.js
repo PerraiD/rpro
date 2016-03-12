4b5b6237b338fb371d8f48632ee7d6ac6a8e2bf4
perraid <davidperrai@gmail.com>
2016-03-12 12:32:59 +0100

    correction de la route du file upload

---
+++ restAPI/fileUploadRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var router      = express.Router();
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+router.post('/upload/file',function(res,req,next){
+    res.send($OPENSHIFT_DATA_DIR);
+});
+
+module.exports = router;

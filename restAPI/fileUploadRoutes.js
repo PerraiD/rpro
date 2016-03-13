089e81f0342fccfa03f97645b703cdf30b287b6a
perraid <davidperrai@gmail.com>
2016-03-13 12:44:33 +0100

    ajout de multer pour parser le fichier uploader

--- restAPI/fileUploadRoutes.js
+++ restAPI/fileUploadRoutes.js
@@ -3,16 +3,23 @@
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var fs = require('fs');
+var upload = multer({ dest: process.env.OPENSHIFT_DATA_DIR});
 var router      = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({
     extended: false
 }));
 
-router.post('/upload/file',function(req,res,next){
-    var rep = JSON.stringify(req);
-res.send(rep);    
-    
+router.post('/upload/file',upload.single('file'),function(req,res,next){    
+    var obj = {
+        body : req.body,
+        file : req.file
+    }
+    res.status(obj);
+
+// res.send(rep);    
+
+//https://github.com/expressjs/multer
     
 //  fs.writeFile(process.env.OPENSHIFT_DATA_DIR+'message.txt', 'Hello Node je te souhaite la bienvenu mon copain', function (err) {
 //     if(err){

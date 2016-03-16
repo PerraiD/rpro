5379b60e80bfe33da0bfa1c5b9196444da8fb097
perraid <davidperrai@gmail.com>
2016-03-16 10:02:13 +0100

    ajout de la requete pour le bouton arduino

---
+++ database/transfertDb.js
@@ -0,0 +1,9 @@
+//suggestion table ,should be replace by a real database for users
+var transfertDb = [
+    {
+          dlink:'',
+          usersTokens:[]     
+    }
+]
+
+module.exports = transfertDb;

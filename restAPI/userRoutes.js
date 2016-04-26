e7d751ebacbefa958a5fd79f5a752b891d5fbe3c
perraid <davidperrai@gmail.com>
2016-04-26 17:16:11 +0200

    debuging du useraround

--- restAPI/userRoutes.js
+++ restAPI/userRoutes.js
@@ -645,8 +645,7 @@ router.get('/', function(req,res,next) {
     
     if(JSON.stringify(user) !== '{}') {        
         userDbStub.forEach(function(otherUser) {          
-            if(user.place.uuid === otherUser.place.uuid  &&  user.id !== otherUser.id ) {
-                res.send('passe');
+            if(user.place.uuid === otherUser.place.uuid  &&  user.id !== otherUser.id ) {                
                 users.push(otherUser);
             }
         }, this);
